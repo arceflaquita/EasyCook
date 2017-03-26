313b6dc2a0829614967a9d202f2ff9bdaff960ba
arceflaquita <colors_des@hotmail.com>
2017-03-26 14:49:15 -0600

    se corrige gadle sync

--- app/src/main/java/com/example/arce/easy_cook/Login.java
+++ app/src/main/java/com/example/arce/easy_cook/Login.java
@@ -1,12 +1,10 @@
 package com.example.arce.easy_cook;
 
-import android.app.Fragment;
 import android.content.Intent;
 import android.net.Uri;
-import android.support.annotation.NonNull;
-import android.support.v4.app.FragmentActivity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,7 +14,6 @@ import android.widget.Toast;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
-import com.facebook.FacebookSdk;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.appindexing.Action;
@@ -27,10 +24,7 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
-import com.google.android.gms.common.api.Api;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.PendingResult;
-import com.google.android.gms.common.api.Status;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.twitter.sdk.android.Twitter;
@@ -39,17 +33,12 @@ import com.twitter.sdk.android.core.Result;
 import com.twitter.sdk.android.core.TwitterAuthConfig;
 import com.twitter.sdk.android.core.TwitterException;
 import com.twitter.sdk.android.core.TwitterSession;
-import com.twitter.sdk.android.core.identity.TwitterAuthClient;
 import com.twitter.sdk.android.core.identity.TwitterLoginButton;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.FileDescriptor;
-import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
-import java.util.Arrays;
-import java.util.concurrent.TimeUnit;
 
 import cz.msebera.android.httpclient.Header;
 import cz.msebera.android.httpclient.HttpEntity;
@@ -147,7 +136,7 @@ public class Login extends AppCompatActivity implements  GoogleApiClient.OnConne
 
         //TODO: Cambiar por la ip de la PC que corre el servicio RestEC en archivo strings.xml
         //ejecutar ipconfig para ver la ip de la maquina
-        urlREST = this.getResources().getString(R.string.urlREST);
+        urlREST = this.getResources().getString(R.string.urlREST);//this.getResources().getString(R.string.urlREST);
 
         editEmail = (EditText) findViewById(R.id.editEmail);
         editContrasena = (EditText) findViewById(R.id.editContrasena);
